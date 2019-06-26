18f8da72bb065f701d088435191a60c7eb6dcbd4
Kirk <kkchu791@gmail.com>
2019-06-26 12:41:48 -0700

    add start method tests for game

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -8,7 +8,8 @@ require_relative "../lib/matrix_renderer.rb"
 
 describe 'TicTacToe' do
   let(:tic_tac_toe) { TicTacToe.new(Board, IOTicTacToe, MatrixRenderer) }
-  
+  let(:io_tic_tac_toe) { IOTicTacToe }
+
   describe 'initialize' do
     context 'when dependencies are specified' do
       it "assigns board_class" do
@@ -22,6 +23,29 @@ describe 'TicTacToe' do
       it "assigns matrix_renderer" do
          expect(tic_tac_toe.instance_variable_get(:@matrix_renderer)).to eq(MatrixRenderer)
       end
+    end
+  end
+
+  describe 'start' do
+    before do
+      allow(io_tic_tac_toe).to receive(:print_welcome_message)
+      allow(tic_tac_toe).to receive(:setup_options)
+      allow(tic_tac_toe).to receive(:run)
+    end
+
+    it 'delegates printing of welcome pessage to io_tic_tac_toe' do
+      expect(io_tic_tac_toe).to receive(:print_welcome_message)
+      tic_tac_toe.start
+    end
+
+    it "runs setup options" do
+      expect(tic_tac_toe).to receive(:setup_options)
+      tic_tac_toe.start
+    end
+
+    it "runs the game" do
+      expect(tic_tac_toe).to receive(:run)
+      tic_tac_toe.start
     end
   end
 end
