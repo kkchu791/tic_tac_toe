609aa19191b29d769075108956076e74aba8530b
Kirk <kkchu791@gmail.com>
2019-06-26 13:03:49 -0700

    add setup_options tests for game

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -9,6 +9,10 @@ require_relative "../lib/matrix_renderer.rb"
 describe 'TicTacToe' do
   let(:tic_tac_toe) { TicTacToe.new(Board, IOTicTacToe, MatrixRenderer) }
   let(:io_tic_tac_toe) { IOTicTacToe }
+  let(:player_1) { Player.new("Tom", "X", 0) }
+  let(:player_2) { Player.new("Tom", "O", 0) }
+  let(:current_player) { player_1 }
+  let(:board) { Board.new }
 
   describe 'initialize' do
     context 'when dependencies are specified' do
@@ -27,6 +31,7 @@ describe 'TicTacToe' do
   end
 
   describe 'start' do
+
     before do
       allow(io_tic_tac_toe).to receive(:print_welcome_message)
       allow(tic_tac_toe).to receive(:setup_options)
@@ -46,6 +51,33 @@ describe 'TicTacToe' do
     it "runs the game" do
       expect(tic_tac_toe).to receive(:run)
       tic_tac_toe.start
+    end
+  end
+
+  describe 'setup_options' do
+    before do
+      allow(io_tic_tac_toe).to receive(:get_players).and_return([player_1, player_2])
+    end
+
+    describe 'set_players' do
+      it "delegates getting players to io_tic_tac_toe" do
+        expect(io_tic_tac_toe).to receive(:get_players)
+        tic_tac_toe.setup_options
+      end
+
+      it "correctly sets the players" do
+        tic_tac_toe.setup_options
+        expect(tic_tac_toe).to have_attributes(player_1: player_1,
+                                               player_2: player_2,
+                                               current_player: current_player)
+      end
+    end
+
+    describe 'create_board' do
+      it "correctly sets the board" do
+        tic_tac_toe.setup_options
+        expect(tic_tac_toe).to have_attributes(board: board)
+      end
     end
   end
 end
